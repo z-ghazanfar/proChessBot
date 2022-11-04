742ff6e5862cf59ba0bd46120cef4523f0f99514
wehaveagod <banerjeeayush05@gmail.com>
2022-11-03 22:59:05 -0400

    Added more files

---
+++ Camera.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import imutils
+
+class Camera:
+
+	def __init__(self):
+		self.cam = PiCamera()
+
+	def takePicture(self):
+
+		# initialize the camera and grab a reference to the raw camera capture
+		rawCapture = PiRGBArray(self.cam)
+
+		# allow the camera to warmup
+		time.sleep(1)
+
+		# grab an image from the camera
+		self.cam.capture(rawCapture, format="bgr")
+		image = rawCapture.array
+		resized = imutils.resize(image, height = 400, width = 400)
+
+
+		return resized
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # proChessBot
+
+ESE 205 Introduction to Engineering Design Project
+
+The goal of the course was to create a new engineering project or implement an existing one online following the given instructions. The wiki page for the course can be found at https://classes.engineering.wustl.edu/ese205/core/index.php?title=Main_Page
+the link for this specific project detailing the implementation can be found at https://classes.engineering.wustl.edu/ese205/core/index.php?title=CV_Chess
